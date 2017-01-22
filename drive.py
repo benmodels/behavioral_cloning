107bc7e04f5ce2fc0cee9f2f19109e3a86e9678c
Behrooz Shahsavari <b.shahsavari@gmail.com>
2017-01-22 12:17:37 -0800

    Added desired driving speed as an optional input argument to drive.py
    Example: use --speed 30 for crazy driving!

--- drive.py
+++ drive.py
@@ -6,14 +6,12 @@ import numpy as np
 import socketio
 import eventlet
 import eventlet.wsgi
-import time
 from PIL import Image
 from PIL import ImageOps
 from flask import Flask, render_template
 from io import BytesIO
 
 from keras.models import model_from_json
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 import cv2
 HEIGHT = 20
 WIDTH = 40
@@ -49,15 +47,15 @@ def telemetry(sid, data):
     #image.save("debug/{0:0>10}.jpg".format(sequence_num))
     
     sequence_num += 1
-    #image_array = image #mage_preprocess(np.asarray(image)).reshape((-1, 16, 32, 1))
-    image_array = cv2.resize(np.asarray(image),(WIDTH,HEIGHT))#/255.0 * 2.0 - 1.0
+    image_array = cv2.resize(np.asarray(image),(WIDTH,HEIGHT))
     image_array = image_array.reshape((1,HEIGHT,WIDTH,CHANNEL))
     
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(image_array, batch_size=1)[0])
     # Very basic way to keep a constant speed
-    if float(speed) < 15:
-        throttle = 0.5
+#    desired_speed = 30.0
+    if float(speed) < desired_speed:
+        throttle = desired_speed/30.
     else:
         throttle = 0.05
     #print(steering_angle, throttle)
@@ -79,18 +77,14 @@ def send_control(steering_angle, throttle):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
+    help='Path to model definition json. Model weights should be on the same path with the same name and .h5 extension.')
+    parser.add_argument('--speed', type=float, default = 15.,
+    help='Desired driving speed between 0 and 30. default = 15')
     args = parser.parse_args()
     with open(args.model, 'r') as jfile:
-        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
-        # then you will have to call:
-        #
-        #   model = model_from_json(json.loads(jfile.read()))\
-        #
-        # instead.
         model = model_from_json(jfile.read())
 
-
+    desired_speed = args.speed
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
     model.load_weights(weights_file)
